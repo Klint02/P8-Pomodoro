3c8900fb18c738d62370d3ba8cda5da6bcdf85e8
Chris31415926 <clma17@student.aau.dk>
2025-04-15 10:32:10 +0200

    Implemented listview + refactoring

--- App/lib/Achievements.dart
+++ App/lib/Achievements.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AchievementsTab extends StatefulWidget {
-  // const AchievementsTab({super.key, required this.title});
   const AchievementsTab({super.key});
-
-  // final String title;
 
   @override
   State<AchievementsTab> createState() => _AchievementsTabState();
@@ -32,78 +29,114 @@ class _AchievementsTabState extends State<AchievementsTab> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Achievements'),
       ),
-      body: Column( children: [Center(
-        child: Column(children: <Widget>[Container(
-          width: 150,
-          // height: 200,
-          // margin: EdgeInsets.all(80),
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.black, width: 2),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              // const Text('You have pushed the button this many times:'),
-              // Text(
-              //   '$_counter',
-              //   style: Theme.of(context).textTheme.headlineMedium,
-              // ),
-              const Text('Streaks:'),
-              const Divider(thickness: 2),
-              for (var item in completedTasks) Text("Completed $item tasks."),
-            ],
-          ),
-        ),
-          Container(
-          width: 150,
-          // height: 200,
-          // margin: EdgeInsets.all(80),
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.black, width: 2),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              // const Text('You have pushed the button this many times:'),
-              // Text(
-              //   '$_counter',
-              //   style: Theme.of(context).textTheme.headlineMedium,
-              // ),
-              const Text('Misc:'),
-              const Divider(thickness: 2),
-              Text('Write something here'),
-              Text('A bit more'),
-              Text('More'),
-              Text('MORE'),
-              Text('...'),
-              Text('Maybe a bit much'),
-            ],
-          ),
-        ),],
-      ),),
-      Row(
+
+      body: Column(
         children: [
-          FilledButton(
-            onPressed: () {_counter = 0;},
-            child: const Text('Reset counter'),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 300,
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          // const Text('You have pushed the button this many times:'),
+                          // Text(
+                          //   '$_counter',
+                          //   style: Theme.of(context).textTheme.headlineMedium,
+                          // ),
+                          const Text('Streaks:'),
+                          const Divider(thickness: 2),
+                          for (var item in completedTasks) Text("Completed $item tasks."),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 300,
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          const Text('Tasks completed:'),
+                          const Divider(thickness: 2),
+                          Text('1 task completed, just getting started.'),
+                          Text('5 tasks completed, getting warmed up.'),
+                          Text('10 tasks completed, now were talking.'),
+                          Text('25 tasks completed, your on fire.'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 300,
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          const Text('Misc:'),
+                          const Divider(thickness: 2),
+                          Text('Write something here'),
+                          Text('A bit more'),
+                          Text('More'),
+                          Text('MORE!'),
+                          Text('...'),
+                          Text('Maybe a bit overkill'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          FilledButton(
-          onPressed: () {_incrementCounter();_addAchievement();},
-          child: const Icon(Icons.add),
-        ),],
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              FilledButton(
+                onPressed: () {_counter = 0;},
+                child: const Text('Reset counter'),
+              ),
+
+              FilledButton(
+                onPressed: () {
+                  _incrementCounter();
+                  _addAchievement();
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
       ),
-      ],),
     );
   }
 }
