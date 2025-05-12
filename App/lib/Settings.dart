6db45c9726f80c4e4170fda94855aba88e30726c
TorbjoernH <thelst20@student.aau.dk>
2025-05-12 14:21:52 +0200

    moved protocols into seperate file

--- App/lib/Settings.dart
+++ App/lib/Settings.dart
@@ -1,73 +1,10 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:async';
 import 'package:pip_boi/taskProvider.dart';
 import 'package:provider/provider.dart';
 import 'package:pip_boi/_task.dart';
-
-Future<bool> getStatus({Duration timeout = const Duration(seconds: 10)}) async {
-  const url = 'http://localhost:3000/status';
-  try {
-    final response = await http.get(Uri.parse(url)).timeout(timeout);
-
-    return response.statusCode == 200;
-  } on TimeoutException {
-    print('Request to get status timed out.');
-  } catch (e) {
-    print("Error: $e");
-  }
-
-  return false;
-}
-
-Future<List<TaskOutput>> receiveTasks(
-    {Duration timeout = const Duration(seconds: 10)}) async {
-  const url = 'http://localhost:3000/receive';
-
-  try {
-    final response = await http.get(Uri.parse(url)).timeout(timeout);
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      print('Received: $data');
-      return data.map((item) => TaskOutput.fromJson(item)).toList();
-    } else {
-      print('Failed to receive tasks');
-      return [];
-    }
-  } on TimeoutException {
-    print('Request to receive timed out.');
-  } catch (e) {
-    print("Error: $e");
-  }
-  return [];
-}
-
-Future<bool> sendTasks(List<Object> inputList,
-    {Duration timeout = const Duration(seconds: 10)}) async {
-  const url = 'http://localhost:3000/send';
-
-  try {
-    final response = await http.post(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(inputList),
-    ).timeout(timeout);
-
-    return response.statusCode == 200;
-  } on TimeoutException {
-    print('Request to receive timed out.');
-  } catch (e) {
-    print("Error: $e");
-  }
-
-  return false;
-}
+import 'package:pip_boi/deviceService.dart';
 
 class SettingsTab extends StatefulWidget {
   const SettingsTab({super.key});
